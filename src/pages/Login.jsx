899cbff8754d60a68eebb42e47236f8727c90711
wendril <wendrild.c.n@gmail.com>
2022-03-25 15:07:30 -0300

    Requisito 2 - Feito com Warnning

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+    state = {
+      nome: '',
+      isDisabled: true,
+      loggedIn: false,
+      isClicked: false,
+    };
+
+  handleTextNome = ({ target: { name, value } }) => {
+    const valueMinName = 3;
+    this.setState({
+      [name]: value,
+      isDisabled: value.length < valueMinName,
+    });
+  }
+
+  handleClick = async () => {
+    const { nome } = this.state;
+    this.setState({
+      isClicked: true,
+    });
+    await createUser({ name: nome });
+    this.setState({
+      loggedIn: true,
+
+    });
+  }
+
   render() {
+    const { nome, isDisabled, loggedIn, isClicked } = this.state;
     return (
       <div data-testid="page-login">
-        Login
+        { isClicked ? (
+          <>
+            <Loading />
+            { loggedIn && <Redirect to="/search" /> }
+          </>
+        ) : (
+          <>
+            Login
+            <label htmlFor="nome">
+              <input
+                id="nome"
+                name="nome"
+                type="text"
+                placeholder="Nome"
+                value={ nome }
+                onChange={ this.handleTextNome }
+                data-testid="login-name-input"
+              />
+            </label>
+            <input
+              id="button"
+              name="button"
+              type="button"
+              value="Entrar"
+              disabled={ isDisabled }
+              data-testid="login-submit-button"
+              onClick={ this.handleClick }
+            />
+          </>
+        )}
       </div>
     );
   }
