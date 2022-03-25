37de8d3cf7ca12f92efb0564ab069f6c710362e8
wendril <wendrild.c.n@gmail.com>
2022-03-25 18:27:59 -0300

    Requisito 5 - Feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isDisabled: true,
+  }
+
+  handleTextNome = ({ target: { name, value } }) => {
+    const valueMinName = 2;
+    this.setState({
+      [name]: value,
+      isDisabled: value.length < valueMinName,
+    });
+  }
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <label htmlFor="pesquisa">
+          <input
+            id="pequisa"
+            name="pesquisa"
+            type="text"
+            placeholder="Nome do Artista"
+            onChange={ this.handleTextNome }
+            data-testid="search-artist-input"
+          />
+        </label>
+        <input
+          id="buttonP"
+          name="buttonP"
+          type="button"
+          value="Pesquisar"
+          disabled={ isDisabled }
+          data-testid="search-artist-button"
+        />
       </div>
     );
   }
