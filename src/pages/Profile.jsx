035c166c5c40ed3c0539558ebf50ba133600aab5
wendril <wendrild.c.n@gmail.com>
2022-03-28 21:35:49 -0300

    Requisito 13 - Feito

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+    loading: true,
+  };
+
+  componentDidMount = async () => {
+    const user = await getUser();
+    this.setState({
+      name: user.name,
+      email: user.email,
+      description: user.description,
+      image: user.image,
+      loading: false,
+    });
+    console.log(user);
+  }
+
   render() {
+    const { name, email, description, image, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        { loading ? (
+          <Loading />
+        ) : (
+          <>
+            <p>{ name }</p>
+            <p>{ email }</p>
+            <p>{ description }</p>
+            <image
+              data-testid="profile-image"
+              src={ image }
+            />
+            <Link
+              to="/profile/edit"
+            >
+              Editar perfil
+            </Link>
+          </>
+        )}
       </div>
     );
   }
