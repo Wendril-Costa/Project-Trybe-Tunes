b15a6d773c0fdc036a25d9c5c41e64ad925c0620
wendril <wendrild.c.n@gmail.com>
2022-03-28 21:08:46 -0300

    Requisit 7 - Feito

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  state = {
+    musics: [],
+  }
+
+  componentDidMount = async () => {
+    const { match: { params: { id } } } = this.props;
+    const idNumber = (id);
+    const musics = await getMusics(idNumber);
+    this.setState({
+      musics,
+    });
+    console.log(musics);
+  }
+
   render() {
+    const { musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <div>
+          {musics.map((element, index) => (
+            (index === 0) ? (
+              <div key={ element.collectionId }>
+                <h4
+                  data-testid="artist-name"
+                >
+                  {element.artistName}
+                </h4>
+                <h4
+                  data-testid="album-name"
+                >
+                  {element.collectionName}
+                </h4>
+              </div>
+            ) : <MusicCard
+              key={ element.trackId }
+              trackName={ element.trackName }
+              previewUrl={ element.previewUrl }
+            />
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Album;
